713d8425d8dc3e60407f5ea24d32c12e3f8431d0
jw-smaal <usenet@gispen.org>
2026-05-24 21:54:15 +0200

    Before refactor for effects

--- src/generators.c
+++ src/generators.c
@@ -20,9 +20,9 @@ const char *get_waveform_name(enum waveform_type type)
 	case WAVE_USR1:
 		return "USR1";
 	case WAVE_USR2:
-		return "USR2";
+		return "FM SINE";
 	case WAVE_USR3:
-		return "USR3";
+		return "NES TRI";
 	case WAVE_USR4:
 		return "USR4";
 	default:
@@ -81,6 +81,34 @@ static inline q15_t calc_usr1(uint32_t phase)
 	return (q15_t)(32768 - (phase >> 16));
 }
 
+/**
+ * @brief Simple Phase Modulation (FM) generator
+ * Modulates carrier phase with a sine at 2x frequency
+ */
+static inline q15_t calc_usr2(uint32_t phase)
+{
+	/* 1. Calculate Modulator (8x frequency harmonic) */
+	q15_t mod = arm_sin_q15((q15_t)((phase * 8) >> 17));
+
+	/* 2. Scale Modulator to create a Phase Offset (Modulation Index) 
+	 * Shifting by 14 gives a rich but controlled metallic timbre.
+	 */
+	uint32_t phase_offset = (uint32_t)mod << 14;
+
+	/* 3. Apply modulated phase to Carrier sine */
+	return arm_sin_q15((q15_t)((phase + phase_offset) >> 17));
+}
+
+/**
+ * @brief NES-style 4-bit (16-step) Triangle wave
+ */
+static inline q15_t calc_usr3(uint32_t phase)
+{
+	q15_t tri = calc_triangle(phase);
+	/* Mask off lower 12 bits to leave only 16 discrete steps (4-bit) */
+	return (q15_t)(tri & 0xF000);
+}
+
 void generate_waveform_batch(enum waveform_type mode, q15_t *buffer, uint32_t size, uint32_t *phase,
 			     uint32_t phase_inc)
 {
@@ -100,6 +128,12 @@ void generate_waveform_batch(enum waveform_type mode, q15_t *buffer, uint32_t si
 			break;
 		case WAVE_USR1:
 			buffer[i] = calc_usr1(*phase);
+			break;
+		case WAVE_USR2:
+			buffer[i] = calc_usr2(*phase);
+			break;
+		case WAVE_USR3:
+			buffer[i] = calc_usr3(*phase);
 			break;
 		default:
 			buffer[i] = 0;
